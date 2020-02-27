0daea82dcb671eddbfa2168c24da38c188d56955
Eduardo <ec@todoo.co>
2020-02-27 22:51:46 +0000

    ADD 27/1

--- academia/models/academia.py
+++ academia/models/academia.py
@@ -2,20 +2,31 @@
 
 from odoo import models, fields
 
+#class academiaEtiqueta(models.Model):
+ #   _name = 'academia.tags'
+ #   _description ='Academia Tags'
+ #   name = fields.Char(string='Name')
 
 class academia(models.Model):
-     _name = 'academia.alumnos'
-     _description = 'Tabla de alumnos'
-
-     name = fields.Char()
-     value = fields.Integer()
-     value2 = fields.Float(compute="_value_pc", store=True)
-     description = fields.Text()
+    _name = 'academia.alumnos'
+    _description = 'Tabla de alumnos'
     
-state = fields.Selection(selection=[('draft', 'New'),('approve', 'Approved'),('confirm', 'Confirmed'),('cancel', 'Cancelled'),('done', 'Done'),], string='State', default='draft')
-
-select = fields.Selection(selection=[('a', 'A')])
-#
+    name = fields.Char(string='Nombre',  required=True, 
+                        index=True, help='Ingres el nombre de alumno.',
+                          readonly=True, states={'approve': [('readonly', False)]})
+    value = fields.Integer(required=True)
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+    active = fields.Boolean(string='Active', default=True)
+    state = fields.Selection(selection=[('draft', 'New'),
+                                        ('approve', 'Approved'),
+                                        ('confirm', 'Confirmed'),
+                                        ('cancel', 'Cancelled'),
+                                        ('done', 'Done'),],
+                             string='State', default='draft')
+    
+    location_id = fields.Many2one(comodel_name='res.partner', ondelete='restrict',readonly=True)
+    #, states={'draft': [('readonly', False)], 'approve': [('readonly', False)]}
 #     @api.depends('value')
 #     def _value_pc(self):
 #         for record in self:
